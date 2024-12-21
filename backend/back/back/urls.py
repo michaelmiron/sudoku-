970b05ed64a8e0caff9e0d69543f3415ceb5325f
Eli_Alhazov <elialhazov20@gmail.com>
2024-12-21 16:00:40 +0200

    remove ×unnecessary path

--- backend/back/back/urls.py
+++ backend/back/back/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("Welcome to the home page!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('check_board/', include('check_board.urls')),
-    path('', home),
 ]

6aebf32aad68eec11a936e5c0444b8d829f960fd
michael_miron <micha767a@gmail.com>
2024-12-24 19:46:23 +0200

    Added a new folder for unit tests for URL resolution and CheckBoard model. Verified URL mapping for save_game and validate_sudoku_board views and validated CheckBoard model creation with correct data handling. also removed the basic default tests.py that was created at the beginning.

---
+++ backend/back/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from check_board.models import CheckBoard
+
+
+class TestCheckBoardModel(TestCase):
+
+    def test_create_check_board_with_valid_data(self):
+        game = CheckBoard.objects.create(
+            time_played="15:30",
+            number_of_mistakes=5
+        )

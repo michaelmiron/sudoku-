fc9725cd0b580691b17bbe85339442f8ebb79c4d
michael_miron <micha767a@gmail.com>
2024-12-24 20:23:03 +0200

    Added another function to test the string representation of the CheckBoard model

--- backend/back/tests/test_models.py
+++ backend/back/tests/test_models.py
@@ -12,3 +12,12 @@ class TestCheckBoardModel(TestCase):
         self.assertEqual(game.time_played, "15:30")
         self.assertEqual(game.number_of_mistakes, 5)
         self.assertIsNotNone(game.game_number)
+
+
+    def test_check_board_str_method(self):
+        game = CheckBoard.objects.create(
+            time_played="12:11",
+            number_of_mistakes=2
+        )
+        expected_str = f"Game {game.game_number}: Time played: 12:11, Mistakes: 2"
+        self.assertEqual(str(game), expected_str)

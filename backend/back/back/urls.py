9481ecefc592ccac84ffb0b5717289de3c18d3fd
Eli_Alhazov <elialhazov20@gmail.com>
2025-01-11 15:43:08 +0200

    Adding graph output using matplotlib when click the end game button gives the details

--- backend/back/back/urls.py
+++ backend/back/back/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('check_board/', include('check_board.urls')),
